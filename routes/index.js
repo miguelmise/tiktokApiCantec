ebe7ee10a3e150c2302e7ef20ddb76d22df0974e
Miguel Mise <mimise@espol.edu.ec>
2022-09-08 09:46:11 -0500

    inicial

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+const TikTokScraper = require('tiktok-scraper');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.send('Analizador de tiktoks CANTEC');
+});
+
+router.post('/video',function(req,res,next){
+  (async () => {
+    try {
+      let link = req.body.enlace;
+        const videoMeta = await TikTokScraper.getVideoMeta(link);
+        res.json(videoMeta);
+    } catch (error) {
+      res.json(error);
+    }
+  })();
+})
+
+module.exports = router;
